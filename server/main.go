c5ff5114e7b20ca06484b4c8f97fd221e8c33468
Broyojo <dha@xoba.com>
2023-07-20 13:03:37 -0400

    added certificates to web server

--- server/main.go
+++ server/main.go
@@ -3,9 +3,19 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
+
+	"github.com/caddyserver/certmagic"
 )
 
 func main() {
-	fileServer := http.FileServer(http.Dir("../public"))
-	log.Fatal(http.ListenAndServe(":8080", fileServer))
+	fs := http.FileServer(http.Dir("../public"))
+	if len(os.Args) >= 2 {
+		if os.Args[1] == "online" {
+			log.Println("Starting online server")
+			log.Fatal(certmagic.HTTPS([]string{"broyojo.com", "www.broyojo.com"}, fs))
+		}
+	}
+	log.Println("Starting local server on port 8080")
+	log.Fatal(http.ListenAndServe(":8080", fs))
 }
